4efedf946548a945ba5f0ba06560b3216e4c9a6a
JustADeer <109961289+JustADeer@users.noreply.github.com>
2022-08-21 14:59:45 +0700

    Add files via upload

--- HomeworkHelper.py
+++ HomeworkHelper.py
@@ -4,36 +4,38 @@ savefile = "data.txt"
 
 
 def AfCommand(command=""):
-    if len(command) >= 2 and str(command[0] + command[1]) == "pr":
-        v = command.replace("pr", "").rstrip()
-        count = 0
-        if exists(savefile) == True:
+    if exists(savefile) == True:
+        if len(command) >= 2 and str(command[0] + command[1]) == "pr":
+            v = command.replace("pr", "").rstrip()
+            count = 0
             data = open(savefile, "r")
-            for line in data.readlines():
-                count += 1
-                Pline = "[{}] : {}".format(count, line.strip())
-                # Checks if what ur finding is in the line
-                if v != "":
+            if v != "":
+                for line in data.readlines():
+                    count += 1
+                    Pline = "[{}] : {}".format(count, line.strip())
+                    # Checks if what ur finding is in the line
                     if v.lower() in Pline.lower():
                         print(Pline)
-                else:
-                    print(Pline)
-        data.close()
-        print("")
+            else:
+                for line in data.readlines():
+                    count += 1
+                    print("[{}] : {}".format(count, line.strip()))
+            print("")
+            data.close()
 
-    elif "+pr" in command:
-        v = command.replace("+pr", "").rstrip()
-        if v != "":
-            if exists(savefile) == True:
+        elif "+pr" in command:
+            v = command.replace("+pr", "").rstrip()
+            if v != "":
                 data = open(savefile, "a")
                 data.write(v + "\n")
                 data.close()
+            else:
+                print("There is no name added. Add a name after like this '+pr name'\n")
 
-    elif "-pr" in command:
-        v = command.replace("-pr", "").rstrip()
-        if v != "":
-            v = int(v) - 1
-            if exists(savefile) == True:
+        elif "-pr" in command:
+            v = command.replace("-pr", "").rstrip()
+            if v != "":
+                v = int(v) - 1
                 data = open(savefile, "r")
                 lines = data.readlines()
                 data.close()
@@ -43,24 +45,26 @@ def AfCommand(command=""):
                     new_file.write(line)
                 new_file.close()
 
-    elif command == "clear":
-        open(savefile, "w").close()
+        elif command == "clear":
+            open(savefile, "w").close()
 
-    elif command == "help":
-        print("\npr .[find],\n+pr [name],\n-pr [index],\nclear,\nquit,\nhelp\n")
+        elif command == "help":
+            print("\npr .[find],\n+pr [name],\n-pr [index],\nclear,\nquit,\nhelp\n")
 
-    elif command == "quit":
-        quit()
+        elif command == "quit":
+            quit()
 
-    else:
-        print("This is Not a Command!\n")
+        elif command == "sF":
+            print(savefile + "\n")
+
+        else:
+            print("This is Not a Command!\n")
 
 
 if __name__ == "__main__":
     if exists(savefile) == False:
-        f = open(savefile, "w")
-        f.close()
-    while True:
+        open(savefile, "w").close()
+    while 1:
         command = input("Command : ")
         print("")
         AfCommand(command)
